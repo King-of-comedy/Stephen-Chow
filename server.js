dc3e82ff81e58990afe0b7db13e49cc0fed4559d
mmmaming <864845434@qq.com>
2017-01-22 17:51:35 +0800

    first

--- server.js
+++ server.js
@@ -10,22 +10,22 @@ var middlewares = jsonServer.defaults();
 server.use(middlewares);
 // Add custom routes before JSON Server router
 server.get('/echo', function (req, res) {
-    res.jsonp(req.query)
+    res.jsonp(req.query);
 });
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use(function (req, res, next) {
     if (req.method === 'POST') {
-        req.body.createdAt = Date.now()
+        req.body.createdAt = Date.now();
     }
     // Continue to JSON Server router
-    next()
+    next();
 });
 
 // Use default router
-server.use(router)
+server.use(router);
 server.listen(9527, function () {
-    console.log('JSON Server is running')
+    console.log('JSON Server is running');
 });
